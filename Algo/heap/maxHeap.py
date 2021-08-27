d2b3f45bc1264deb1907eea50aff2b93bc076a61
YuseonChoi <chldbtjs74@gmail.com>
2021-08-27 23:26:50 +0900

    최대힙

--- Algo/heap/maxHeap.py
+++ Algo/heap/maxHeap.py
@@ -17,3 +17,9 @@ while True:
             print(-hp.heappop(nums))
     else:
         hp.heappush(nums, -n)
+
+
+"""
+파이썬 heapq 모듈은 최소 힙만 지원하기 때문에 최대 힙을 구현하려면 음수로 저장한 다음 가장 낮은 수부터 추출해 부호를 변환해야 한다.
+
+"""
