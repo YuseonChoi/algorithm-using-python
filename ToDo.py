391ea9064e26e793ad24c73adf1c933a404de983
YuseonChoi <chldbtjs74@gmail.com>
2021-10-02 23:52:56 +0900

    10/2 보충필요

--- ToDo.py
+++ ToDo.py
@@ -3,10 +3,10 @@
 LeetCode/349_intersection.py
 Class/numSequence.py
 Baekjoon/1654.py
-Baekjoon/2745.py
 Baekjoon/11005.py
 LeetCode/406_height.py
 Baekjoon/1918.py
 Programmers/findPrime.py 재귀 이해하기
+LeetCode/15.py
 
 """
