db1cc514a724cf7589f7cbf6313e0419cea49b9c
YuseonChoi <chldbtjs74@gmail.com>
2025-04-02 15:57:03 +0900

    [그래프] DFS 구현

---
+++ CodingTest/그래프/dfs.py
@@ -0,0 +1,27 @@
+"""
+[깊이 우선 탐색 구현]
+시스템 스택을 이용한 재귀 함수로 구현
+"""
+
+def dfs(current_node):
+    # 현재 노드 방문
+    visited[current_node] = True
+    print(current_node)
+
+    # 인접한 노드 탐색
+    for adj_node in graph[current_node]:
+        # 방문하지 않았다면 호출
+        if not visited[adj_node]:
+            dfs(adj_node)
+
+graph = {
+    1: [4,5],
+    2: [3],
+    3: [],
+    4: [2,3],
+    5: [4]
+}
+
+visited = [False for i in range(len(graph)+1)]
+
+dfs(1)  # 1,4,2,3,5 
