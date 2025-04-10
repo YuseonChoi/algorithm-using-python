ccb96bf27e6e782558d2e8b9700426c3b8e8bcc9
YuseonChoi <chldbtjs74@gmail.com>
2025-04-10 13:27:08 +0900

    [동적계획법] 피보나치 수

--- CodingTest/동적계획법/12945.py
+++ CodingTest/동적계획법/12945.py
@@ -3,5 +3,7 @@
 def solution(n):
     fibo_arr = [0,1]
     for i in range(2, n+1):
-        fibo_arr.append((fibo_arr[i-2] + fibo_arr[i-1])%1234567)
+        fibo_arr.append(
+            (fibo_arr[i-1] + fibo_arr[i-2])%1234567
+            )
     return fibo_arr[n]
