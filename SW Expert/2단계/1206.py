1966aedc210d1877f95c8dedc6d4a715fc70dd68
YuseonChoi <chldbtjs74@gmail.com>
2025-08-25 17:25:43 +0900

    1206. View

--- SW Expert/2단계/1206.py
+++ SW Expert/2단계/1206.py
@@ -1,6 +1,7 @@
 import sys
 sys.stdin = open('input_1206.txt', 'r')
 
+""" solution 1 - 내 풀이 """
 for t in range(1, 11):
     n = int(input())
     arr = list(map(int, input().split()))
@@ -20,3 +21,22 @@ for t in range(1, 11):
             if num_val > 0: cnt += num_val
 
     print(f"#{t}", cnt)
+
+
+""" solution 2 - 참고용 """
+T = 10
+for test_case in range(1, T+1):
+    N = int(input())
+    lst = list(map(int, input().split()))
+    ans = 0
+    for i in range(2, N-2):
+        mx = lst[i-2]
+        for j in range(i-1, i+3):
+            if j == i:
+                continue
+            else:
+                if mx < lst[j]:
+                    mx = lst[j]
+        if lst[i] > mx:
+            ans += lst[i] - mx
+    print(f"#{test_case}", ans)
