075d51191e70d214354999ae2dcf225b3ca4e578
YuseonChoi <chldbtjs74@gmail.com>
2025-08-26 15:51:26 +0900

    14501. 퇴사

--- Baekjoon/14501.py
+++ Baekjoon/14501.py
@@ -60,6 +60,21 @@ dfs(0, 0)
 print(ans)
 
 
+""" solution 2 - DP """
+N = int(input())
+T = [0]*N
+P = [0]*N
+dp = [0]*(N+1)
+for i in range(N):
+    T[i], P[i] = map(int, input().split())
+
+for n in range(N-1, -1, -1):  # 뒤에서 앞으로(완료 기준)
+    if n+T[n] <= N:  # 기간 내 상담 완료 가능
+        dp[n] = max(dp[n+1], dp[n+T[n]]+P[n])
+    else:
+        dp[n] = dp[n+1]
+
+print(dp[0])
 
 
 
