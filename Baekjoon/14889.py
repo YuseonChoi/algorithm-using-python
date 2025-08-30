0bbecb5d29d0a0a42ea53273be6eec9d99d96bda
YuseonChoi <chldbtjs74@gmail.com>
2025-08-30 13:20:23 +0900

    14889. 스타트와 링

--- Baekjoon/14889.py
+++ Baekjoon/14889.py
@@ -36,6 +36,44 @@ for comb in all_combs:
 print(min_score)
 
 
+"""
+solution - 백트래킹
+* 이진 트리 형태로 구성한다면 2^20 = 10^6
+* yes no 형태의 이진트리인지 멀티트리인지 복잡도 계산해보기
+* 다양한 아이디어를 내기 보다는 백트래킹으로 풀이해보는 게 좋음
+* 백트래킹 : 가능한 모든 경우
+"""
+
+# 빈번하게 계산을 해야한다면 함수 호출보다는 dfs 안에 구성하는 게 빠를 수 있음
+# 가지치기는 호출 횟수는 줄일 수 있으나 실행 시간을 줄일 수 있을지 여부는 알 수 없 -> 테스트케이스에 따라 다름
+def cal(alst, blst):
+    asm = 0
+    bsm = 0
+    for i in range(0, M):
+        for j in range(0, M):
+            asm += arr[alst[i]][alst[j]]
+            bsm += arr[blst[i]][blst[j]]
+    return abs(asm - bsm)
+
+def dfs(n, alst, blst):
+    global ans
+    if n == N:
+        if len(alst) == len(blst):  # 같은 인원 수로 팀을 구성
+            ans = min(ans, cal(alst, blst))
+        return
+
+    # A팀 선택
+    dfs(n+1, alst+[n], blst)
+    # B팀 선택
+    dfs(n + 1, alst, blst+[n])
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+M = N//2
+ans = 100*M*M
+dfs(0, [], [])
+print(ans)
 
 
 
