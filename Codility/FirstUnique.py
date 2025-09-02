96698a8eefbb2cc5d7c28d7cd53aa7889cbb2849
YuseonChoi <chldbtjs74@gmail.com>
2025-09-02 10:49:12 +0900

    FirstUnique

--- Codility/FirstUnique.py
+++ Codility/FirstUnique.py
@@ -1,8 +1,12 @@
+"""
+쉬운 문제인데 효율성 생각하느라 애먹음..
+딕셔너리, 리스트 컴프리헨션 활용법 숙지하기!
+"""
+
 def solution(A):
-    # Implement your solution here
     dict_cnt = {}
     for a in A:
-        dict_cnt[a] = dict_cnt.get(a, 0)+1
+        dict_cnt[a] = dict_cnt.get(a, 0)+1  # 값이 있으면 원래 값, 없으면 0 반환
     result = [a for a in A if dict_cnt[a]==1]
     if len(result) == 0:
         return -1
