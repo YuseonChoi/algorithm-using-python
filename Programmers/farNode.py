6515a83125e5776cf29788964d2c1a6f5a2bb1c8
YuseonChoi <chldbtjs74@gmail.com>
2025-09-12 11:05:02 +0900

    가장 먼 노드

--- Programmers/farNode.py
+++ Programmers/farNode.py
@@ -3,29 +3,29 @@
 from collections import defaultdict, deque
 
 def bfs(start, graph, n):
-    # 거리 배열 초기화 (-1 -> 방문 X)
-    dist = [-1] * (n+1)
+    dist = [-1]*(n+1)
     dist[start] = 0
-    
     q = deque([start])
     
     while q:
         node = q.popleft()
         for next in graph[node]:
+            # 한 번도 방문한 적 없는 노드라면,
             if dist[next] == -1:
                 dist[next] = dist[node] + 1
                 q.append(next)
                 
     return dist
+                 
         
 def solution(n, edge):
-    graph = defaultdict(list)
+    answer = 0
     
+    graph = defaultdict(list)
     for n1, n2 in edge:
         graph[n1].append(n2)
         graph[n2].append(n1)
-            
-    dist = bfs(1, graph, n)  # 1번 노드부터 BFS
-    max_dist = max(dist[1:])  # 가장 먼 거리
-
-    return dist.count(max_dist)  # 가장 먼 거리에 있는 노드 개수
+        
+    dist = bfs(1, graph, n)
+        
+    return dist.count(max(dist))
