a8fa91f05deb3b635d45111f88a5d1e41ff03427
YuseonChoi <chldbtjs74@gmail.com>
2025-09-21 20:24:03 +0900

    가장 큰 수

--- Programmers/biggestNum.py
+++ Programmers/biggestNum.py
@@ -3,7 +3,11 @@
 def solution(numbers):
     answer = ''
     
+    # 사전 순으로 더 큰 수가 오도록 정렬
+    # 가장 큰 수는 네 자리 수 이하 
+    # 6과 34의 경우, 6666 > 34343434
     sort_nums = sorted(list(map(str,numbers)), key = lambda x: x*4, reverse=True)
     answer = ''.join(sort_nums)
 
+    # 000이 아니라 0으로 나와야 함 
     return str(int(answer))
