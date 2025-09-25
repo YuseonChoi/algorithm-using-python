ee8a1d6e5a1939f4f53cd474318c2bd24aafb87e
YuseonChoi <chldbtjs74@gmail.com>
2025-09-25 14:57:53 +0900

    전화번호 목록

--- Programmers/phoneBookList.py
+++ Programmers/phoneBookList.py
@@ -1,6 +1,6 @@
 """ <전화번호 목록> https://school.programmers.co.kr/learn/courses/30/lessons/42577 """
 
-
+""" solution 1 """
 def solution(phone_book):
     phone_book.sort()
     
@@ -9,3 +9,20 @@ def solution(phone_book):
             return False
 
     return True
+
+
+""" solution 2 """
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+        
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
